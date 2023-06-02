f5a176c557ee306bbc72e2f9f7c30def802954e2
Anthony Khajira <anthonykhajira@Anthonys-MacBook-Air.local>
2023-06-02 11:45:24 +0300

    Finalization of the bottom Component

--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -7,7 +7,6 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 //Color for the Text
-val DarkBlue= Color(0xFF232751)
-
-val VeryLightGray =Color(0xFF) // COLOR FOR THE BACKGROUND
-val LightGray =Color(0xFFF3F2F5) //COLOR FOR THE DIVIDER
+val DarkBlue = Color(0xFF232751)
+val VeryLightGray = Color(0xFFF8FAFB)
+val LightGray = Color(0xFFF3F2F5)
